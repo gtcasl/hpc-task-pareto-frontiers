eebcabc7e17a1d794e79119ae90e2783b5f6d226
Eric Anger <eanger@gmail.com>
2016-02-09 14:47:25 -0500

    make sure all allocations are 64 byte aligned

--- src/blas.cc
+++ src/blas.cc
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <memory>
 #include <unistd.h>
 #include <vector>
 
@@ -13,8 +14,6 @@
 // compile with -mkl
 // link with -lmkl_rt
 
-#define NUM_ITERS 1000
-
 using namespace std;
 
 void rand_fill(double* a,int n){
@@ -23,33 +22,51 @@ void rand_fill(double* a,int n){
   }
 }
 
-double gen_rand(){
-  return ((double)rand()/(double)RAND_MAX);
-}
+struct mklfreer{
+  void operator()(double* d){
+    mkl_free((void*) d);
+  }
+};
+
+struct mklvec{
+  unique_ptr<double,mklfreer> v;
+  mklvec(int n){
+    v.reset((double*)mkl_malloc(n * sizeof(double), 64));
+  }
+  double* data(){
+    return v.get();
+  }
+};
 
 // Cholesky
 // DPOTRF DTRSM DSYRK DGEMM
 struct dpotrf_t{
-  vector<double> a;
+  mklvec a;
   int n;
-  dpotrf_t(int N) : a(N * N), n(N){
-    generate(a.begin(), a.end(), gen_rand);
-    vector<double> a_copy = a;
-    vector<double> sym_mat(N * N);
-    mkl_domatadd('r', 'n', 't', N, N, 1.0, &a.front(), N, 1.0,
-                 &a_copy.front(), N, &sym_mat.front(), N);
-
-    vector<double> ident(N*N, 0.0);
-    for(int i = 0; i < N; i++){
-      ident[i * N + i] = N;
+  dpotrf_t(int N) : a(N*N), n(N){
+    rand_fill(a.data(), N*N);
+    mklvec a_copy(N*N);
+    for(int i = 0; i < N*N; i++){
+      a_copy.data()[i] = a.data()[i];
     }
-    mkl_domatadd('r', 'n', 'n', N, N, 1.0, &a_copy.front(), N, 1.0,
-                 &ident.front(), N, &a.front(), N);
+    mklvec sym_mat(N*N);
+    mkl_domatadd('r', 'n', 't', N, N, 1.0, a.data(), N, 1.0, a_copy.data(), N,
+                 sym_mat.data(), N);
+
+    mklvec ident(N*N);
+    for(int i = 0; i < N*N; i++){
+      ident.data()[i] = 0.0;
+    }
+    for(int i = 0; i < N; i++){
+      ident.data()[i * N + i] = N;
+    }
+    mkl_domatadd('r', 'n', 'n', N, N, 1.0, a_copy.data(), N, 1.0, ident.data(),
+                 N, a.data(), N);
   }
 };
 
-void run_dpotrf(dpotrf_t d){
-  int info = LAPACKE_dpotrf(LAPACK_ROW_MAJOR, 'U', d.n, &d.a.front(), d.n);
+void run_dpotrf(dpotrf_t& d){
+  int info = LAPACKE_dpotrf(LAPACK_ROW_MAJOR, 'U', d.n, d.a.data(), d.n);
   if(info != 0){
     cerr << "Error running dpotrf" << endl;
     exit(-1);
@@ -57,103 +74,104 @@ void run_dpotrf(dpotrf_t d){
 }
 
 struct dtrsm_t{
-  vector<double> a,b;
+  mklvec a,b;
   int n;
   dtrsm_t(int N) : a(N*N), b(N*N), n(N){
-    generate(a.begin(), a.end(), gen_rand);
-    generate(b.begin(), b.end(), gen_rand);
+    rand_fill(a.data(), N*N);
+    rand_fill(b.data(), N*N);
   }
 };
 
-void run_dtrsm(dtrsm_t d){
+void run_dtrsm(dtrsm_t& d){
   cblas_dtrsm(CblasRowMajor, CblasRight, CblasUpper, CblasNoTrans, CblasUnit,
-              d.n, d.n, 1.0, &d.a.front(), d.n, &d.b.front(), d.n);
+              d.n, d.n, 1.0, d.a.data(), d.n, d.b.data(), d.n);
 }
 
 struct dsyrk_t{
-  vector<double> a,c;
+  mklvec a,c;
   int n;
   dsyrk_t(int N) : a(N*N), c(N*N), n(N){
-    generate(a.begin(), a.end(), gen_rand);
-    generate(c.begin(), c.end(), gen_rand);
+    rand_fill(a.data(), N*N);
+    rand_fill(c.data(), N*N);
   }
 };
 
-void run_dsyrk(dsyrk_t d){
+void run_dsyrk(dsyrk_t& d){
   cblas_dsyrk(CblasRowMajor, CblasUpper, CblasNoTrans, d.n, d.n, 1.0,
-              &d.a.front(), d.n, 1.0, &d.c.front(), d.n);
+              d.a.data(), d.n, 1.0, d.c.data(), d.n);
 }
 
 struct dgemm_t{
-  vector<double> a,b,c;
+  mklvec a,b,c;
   int n;
   dgemm_t(int N) : a(N*N), b(N*N), c(N*N), n(N){
-    generate(a.begin(), a.end(), gen_rand);
-    generate(b.begin(), b.end(), gen_rand);
+    rand_fill(a.data(), N*N);
+    rand_fill(b.data(), N*N);
   }
 };
 
-void run_dgemm(dgemm_t d){
+void run_dgemm(dgemm_t& d){
   cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, d.n, d.n, d.n, 1.0,
-              &d.a.front(), d.n, &d.b.front(), d.n, 0, &d.c.front(), d.n);
+              d.a.data(), d.n, d.b.data(), d.n, 0, d.c.data(), d.n);
 }
 
 // CG
 // DGEMV DAXPY DDOT
 struct dgemv_t{
-  vector<double> x,a,y;
+  mklvec x,a,y;
   int n;
   dgemv_t(int N) : x(N), a(N*N), y(N), n(N){
     // initialize data with garbage or whatever
-    generate(x.begin(), x.end(), gen_rand);
-    generate(a.begin(), a.end(), gen_rand);
+    rand_fill(x.data(), N);
+    rand_fill(a.data(), N*N);
   }
 };
 
-void run_dgemv(dgemv_t d){
-  cblas_dgemv(CblasRowMajor, CblasNoTrans, d.n, d.n, 1, &d.a.front(), d.n,
-              &d.x.front(), 1, 0, &d.y.front(), 1);
+void run_dgemv(dgemv_t& d){
+  cblas_dgemv(CblasRowMajor, CblasNoTrans, d.n, d.n, 1, d.a.data(), d.n,
+              d.x.data(), 1, 0, d.y.data(), 1);
 }
 
 struct daxpy_t{
-  vector<double> x,y;
+  mklvec x,y;
   double a;
   int n;
   daxpy_t(int N) : x(N*N), y(N*N), a(43.59), n(N*N){
-    generate(x.begin(), x.end(), gen_rand);
-    generate(y.begin(), y.end(), gen_rand);
+    rand_fill(x.data(), N*N);
+    rand_fill(y.data(), N*N);
   }
 };
 
-void run_daxpy(daxpy_t d){
-  cblas_daxpy(d.n, d.a, &d.x.front(), 1, &d.y.front(), 1);
+void run_daxpy(daxpy_t& d){
+  cblas_daxpy(d.n, d.a, d.x.data(), 1, d.y.data(), 1);
 }
 
 struct ddot_t{
-  vector<double> x,y;
+  mklvec x,y;
   int n;
   ddot_t(int N) : x(N*N), y(N*N), n(N*N){
-    generate(x.begin(), x.end(), gen_rand);
-    generate(y.begin(), y.end(), gen_rand);
+    rand_fill(x.data(), N*N);
+    rand_fill(y.data(), N*N);
   }
 };
 
-void run_ddot(ddot_t d){
-  cblas_ddot(d.n, &d.x.front(), 1, &d.y.front(), 1);
+void run_ddot(ddot_t& d){
+  cblas_ddot(d.n, d.x.data(), 1, d.y.data(), 1);
 }
 
 template<typename T>
-void experiment(lwperf_t perf, const char* name, void(*run_fn)(T), int N){
+void experiment(lwperf_t perf, const char* name, void(*run_fn)(T&), int N,
+                int num_iters){
   cout << "Running experiment for " << name << endl;
   vector<T> prepped;
-  for(int i = 0; i < NUM_ITERS + 1; i++){
-    prepped.push_back(T(N));
+  for(int i = 0; i < num_iters + 1; i++){
+    prepped.emplace_back(N);
   }
 
   // warm up
-  run_fn(prepped[NUM_ITERS]);
+  run_fn(prepped[num_iters]);
   lwperf_log(perf, name);
-  for(int i = 0; i < NUM_ITERS; i++){
+  for(int i = 0; i < num_iters; i++){
     run_fn(prepped[i]);
   }
   lwperf_stop(perf, name);
@@ -176,14 +194,14 @@ int main(int argc, char* argv[]){
 
 // DPOTRF DTRSM DSYRK DGEMM
 // DGEMV DAXPY DDOT
-#define EXPERIMENT(name) experiment(perf, #name, run_ ## name, N)
-  EXPERIMENT(dpotrf);
-  EXPERIMENT(dtrsm);
-  EXPERIMENT(dsyrk);
-  EXPERIMENT(dgemm);
-  EXPERIMENT(dgemv);
-  EXPERIMENT(daxpy);
-  EXPERIMENT(ddot);
+#define EXPERIMENT(name,iters) experiment(perf, #name, run_ ## name, N, iters)
+  EXPERIMENT(dpotrf,50);
+  EXPERIMENT(dtrsm,2000);
+  EXPERIMENT(dsyrk,400);
+  EXPERIMENT(dgemm,500);
+  EXPERIMENT(dgemv,4000);
+  EXPERIMENT(daxpy,10000);
+  EXPERIMENT(ddot,10000);
 #undef EXPERIMENT
 
   lwperf_finalize(perf);
