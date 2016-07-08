9b89c28193b4082ffea0b9f010cc276156279430
Eric Anger <eanger@gmail.com>
2016-07-08 13:46:12 -0400

    initial power budget experiments

--- runtime/experiment.sh
+++ runtime/experiment.sh
@@ -3,15 +3,18 @@
 #export NUMTHREADS=228
 #export POWERLIMIT=110
 
-./cg.sh
-./cholesky.sh
-tail -n 3 scheduler.cg.log > cg.power.log
-tail -n 3 scheduler.cholesky.log > cholesky.power.log
-
-for x in 1 5 10 20 30 40 50 60
+for y in `seq 1 10`
 do
-    POWERLIMIT=$x ./cg.sh
-    POWERLIMIT=$x ./cholesky.sh
-    tail -n 3 scheduler.cg.log >> cg.power.log
-    tail -n 3 scheduler.cholesky.log >> cholesky.power.log
+    #for x in 1 5 10 20 30 40 50 60
+    #do
+    #    POWERLIMIT=$x ./cg.sh
+    #    POWERLIMIT=$x ./cholesky.sh
+    #    tail -n 3 scheduler.cg.log >> cg.$x.log
+    #    tail -n 3 scheduler.cholesky.log >> cholesky.$x.log
+    #done
+    #./cg.sh
+    ./cholesky.sh
+    #tail -n 3 scheduler.cg.log >> cg.none.log
+    tail -n 3 scheduler.cholesky.log >> cholesky.none.log
 done
+
