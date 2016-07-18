c99e0f682afb4128059fd759948b26899eaa75dc
Eric Anger <eanger@gmail.com>
2016-07-18 08:32:17 -0400

    add results for time-based profiling of cg

---
+++ experiments/without-cpufreq/parse.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+for i in range(1,229):
+    fname = "cg." + str(i) + ".log"
+    with open(fname, 'r') as f:
+        for line in f:
+            if "elapsed_seconds" in line:
+                d = {}
+                for e in line.split(',')[:-1]:
+                    [key,val] = e.split('=')
+                    d[key] = val
+                elapsed = d['elapsed_seconds']
+                name = d['name']
+                avgpow = d['avg_power_W']
+                maxpow = d['max_power_W']
+                with open(name + ".csv", 'a') as res:
+                    res.write(str(i) + "," + elapsed + "," + avgpow + "," + maxpow + "\n")
