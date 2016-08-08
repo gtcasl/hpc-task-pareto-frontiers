89289cb0e5b97bfc8528c52432f2a60affe9996d
Eric Anger <eanger@gmail.com>
2016-08-08 15:20:40 -0400

    add most up to date cholesky profiling resutls

---
+++ experiments/cholesky-profile/to-csv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for x in gemm syrk potrf trsm
+do
+    infile=${x}profile.avg.csv
+    outfile=${x}.csv
+    tmpfile=${x}.tmp
+    tail -n +2 ../$outfile > $tmpfile
+    echo "nthreads,energy,time,power,speedup" > $outfile
+    paste -d ',' $tmpfile $infile | awk -F "," 'BEGIN {OFS=","} {print $1,$2,$9,$7,$5}' >> $outfile
+    rm $tmpfile
+done
