3d9da195c89fc27e33d9575c8ac7d013e08a58df
Eric Anger <eanger@gmail.com>
2016-08-11 14:24:07 -0400

    remove some old scripts we don't use any more

--- runtime/cg.profile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#cg
-./go.sh 2 -s profile cg 200 200 200 16
-mv profile.log profile.cg.log
--- runtime/cholesky.profile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#cholesky
-./run -s profile choleskyprofiling 6 1400
-mv scheduler.log profile.cholesky.log
